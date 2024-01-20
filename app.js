6cae5db7811f3cff11895c8dc14b120098fc9a40
leanne dozier <leannedoja@gmail.com>
2024-01-20 03:29:11 -0700

    completed operators exercise

--- app.js
+++ app.js
@@ -7,13 +7,54 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+var a = 20;
+var b = 4;
+var add = a + b;    //24
+var minus = a - b;   //16
+var multiply = a * b;  //80 
+var divide = a / b;    //5
+
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;   
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+ ////What is the value of: num + str?              11 + "11" = "1111"
+ ////What is the value of: num + str2?             11 + "eleven" = "11eleven"
+ ////What is the value of: num + isPresent?        11 + true = 12
+ ////What is the value of: firstName + num?        "Frodo" + 11 = "Frodo11"
+ ////What is the value of: isPresent + str?        true + "11" = "true11"
+ ////What is the value of: firstName + lastName?   "Frodo" + "Baggins" = "FrodoBaggins"
+console.log(num + str); 
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+//What is the value of: val == str3?        5 == "5"     true
+ //What is the value of: val === str3?      5 === "5"    false
+ //What is the value of: !isAwake?          !isAwake = true
+ //What is the value of: ("eleven" == str4 && val >= str3)?    false
+ //What is the value of: (!isAwake || isAwake)?   true
+ //What is the value of: 0 == false?            true
+ //What is the value of: 0 === false?          false
+ //What is the value of: 0 != false?           false
+ //What is the value of 0 !== false?           true
